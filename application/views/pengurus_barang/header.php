cf1279931f427ed5d3bf613ec0d9e368fdc8a91d
ziamulyana <ziamulyana13@gmail.com>
2019-05-23 23:50:42 +0700

    tes fak yu

--- application/views/pengurus_barang/header.php
+++ application/views/pengurus_barang/header.php
@@ -45,7 +45,7 @@
 
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>BPKAD | Pengurus Barang</title>
+  <title>fak yu | Pengurus Barang</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?php echo base_url()?>assets/bootstrap/css/bootstrap.min.css">
