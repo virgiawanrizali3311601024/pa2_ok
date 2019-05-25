bb35c0f969426ab3b0b72837e64064a8ca3b3427
virgiawan rizali <36495414+virgiawanrizali3311601024@users.noreply.github.com>
2019-05-25 16:27:44 +0700

    skurt heder

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>BPKAD | Staf Aset</title>
+  <title>BPKAD | Admin</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
   <!-- Bootstrap 3.3.7 -->
@@ -17,17 +17,6 @@
   <link rel="stylesheet" href="<?php echo base_url()?>assets/bower_components/datatables/css/dataTables.bootstrap.css">
   <link rel="stylesheet" href="<?php echo base_url()?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.css">
   
-
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load.   
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/bower_components/morris.js/morris.css">
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/bower_components/jvectormap/jquery-jvectormap.css">    
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">    
-  -->
-
-  
-  
-
   <!-- jQuery 3 -->
   <script src="<?php echo base_url()?>assets/bower_components/jquery/dist/jquery.min.js"></script>
   <!-- jQuery UI 1.11.4 -->
@@ -66,8 +55,8 @@
 <div class="wrapper">
 
   <header class="main-header">
-    <!-- Logo -->
-    <a href="<?php echo base_url().'pengurus_barang/index'?>" class="logo">
+     <!-- Logo -->
+    <a class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini" style="font-size:11px;"><b>BPKAD</b></span>
       <!-- logo for regular state and mobile devices -->
@@ -76,15 +65,13 @@
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
       <!-- Sidebar toggle button-->
-      <a href="<?php echo base_url()?>assets/#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
+      
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
-            <a href="<?php echo base_url()?>assets/#" class="dropdown-toggle" data-toggle="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown">
               <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs">   </span>
             </a>
@@ -92,16 +79,15 @@
               <!-- User image -->
               <li class="user-header">
                 <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
                 <p>
-                   
-                  <small>Kode Lokasi :    </small>
+                  <p><?php echo $this->session->userdata('nama');?></p>
+                  <p><?php echo $this->session->userdata('tipe_user');?></p>
                 </p>
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="<?php echo base_url()?>admin/gantipassword" class="btn btn-default btn-flat"><i class="fa fa-cogs"></i> Password</a>
+                  <a href="<?php echo base_url()?>pengurus_barang/gantipassword" class="btn btn-default btn-flat"><i class="fa fa-cogs"></i> Kata Sandi</a>
                 </div>
                 <div class="pull-right">
                   <a href="<?php echo site_url('login/logout') ?>" class="btn btn-default btn-flat"><i class="fa fa-sign-out"></i> Keluar</a>
@@ -128,36 +114,8 @@
       </div>
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MAIN NAVIGATION</li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-hashtag"></i> <span>Data Masukan</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <!--
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url().'admin/masukan_kiba'?>"><i class="fa fa-circle-o"></i> KIB A</a></li>
-            <li><a href="<?php echo base_url().'admin/masukan_kibb'?>"><i class="fa fa-circle-o"></i> KIB B</a></li>
-            <li><a href="<?php echo base_url().'admin/masukan_kibc'?>"><i class="fa fa-circle-o"></i> KIB C</a></li>
-            <li><a href="<?php echo base_url().'admin/masukan_kibd'?>"><i class="fa fa-circle-o"></i> KIB D</a></li>
-            <li><a href="<?php echo base_url().'admin/masukan_kibe'?>"><i class="fa fa-circle-o"></i> KIB E</a></li>
-            <li><a href="<?php echo base_url().'admin/masukan_kibf'?>"><i class="fa fa-circle-o"></i> KIB F</a></li>
-          </ul>
-          -->
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-folder"></i> <span>User</span>
-            <span class="pull-right-container"> 
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url().'admin/ks_bpkad'?>"><i class="fa fa-circle-o"></i> Ketua & Staff BPKAD</a></li>
-            <li><a href="<?php echo base_url().'admin/pengurus_barang'?>"><i class="fa fa-circle-o"></i> Pengurus Barang</a></li>
-          </ul>
+        <li class="header">NAVIGASI</li>
+            <li><a href="<?php echo base_url().'admin/ks_bpkad'?>"><i class="fa fa-address-book"></i> Kelola Pengguna</a></li>
         </li>
       </ul>
     </section>
--- application/views/staff_bpkad/header.php
+++ application/views/staff_bpkad/header.php
@@ -65,8 +65,8 @@
 <div class="wrapper">
 
   <header class="main-header">
-    <!-- Logo -->
-    <a href="<?php echo base_url().'staff_bpkad/index'?>" class="logo">
+     <!-- Logo -->
+    <a class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini" style="font-size:11px;"><b>BPKAD</b></span>
       <!-- logo for regular state and mobile devices -->
@@ -75,15 +75,13 @@
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
       <!-- Sidebar toggle button-->
-      <a href="<?php echo base_url()?>assets/#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
+      
 
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
-            <a href="<?php echo base_url()?>assets/#" class="dropdown-toggle" data-toggle="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown">
               <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs">   </span>
             </a>
@@ -91,15 +89,18 @@
               <!-- User image -->
               <li class="user-header">
                 <img src="<?php echo base_url()?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <p>
+                  <p><?php echo $this->session->userdata('nama');?></p>
+                  <p><?php echo $this->session->userdata('tipe_user');?></p>
+                </p>
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="<?php echo base_url()?>staff_bpkad/gantipassword" class="btn btn-default btn-flat"><i class="fa fa-cogs"></i> Password</a>
+                  <a href="<?php echo base_url()?>pengurus_barang/gantipassword" class="btn btn-default btn-flat"><i class="fa fa-cogs"></i> Kata Sandi</a>
                 </div>
                 <div class="pull-right">
                   <a href="<?php echo site_url('login/logout') ?>" class="btn btn-default btn-flat"><i class="fa fa-sign-out"></i> Keluar</a>
-                </div>
               </li>
             </ul>
           </li>
